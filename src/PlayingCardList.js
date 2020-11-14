293984e9547ded290ae210d6dd6bcefa41c0ab07
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-11-13 17:43:24 -0800

    refactor

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -14,7 +14,7 @@ function CardTable() {
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={() => getCards()}>Add a playing card!</button>
+        <button onClick={(evt) => getCards()}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
